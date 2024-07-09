fb7fce903e8d32d2264eb52e7b6b140f5377855f
Memo Martell <gucasta@me.com>
2024-07-09 12:19:43 -0600

    End of section 4

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { getMembers } from "../actions/membersActions";
+import MemberCard from "./MemberCard";
 
-export default function MembersPage() {
-  return (
-    <div>Members Page</div>
-  )
+export default async function MembersPage() {
+	const members = await getMembers();
+
+	return (
+		<div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+			{members &&
+				members.map((member) => (
+					<MemberCard member={member} key={member.id} />
+				))}
+		</div>
+	);
 }
